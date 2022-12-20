eaf81a489c077fcca31e97e6b465827d9fd3d8f9
D4riu5 <R3iji94@gmail.com>
2022-12-20 21:18:11 +0100

    Milestone 2 completed

--- js/script.js
+++ js/script.js
@@ -5,9 +5,8 @@ const generate = document.querySelector('.generate-ticket');
 const age = document.getElementById("age-range")
 let ticketPrice
 
-generate.addEventListener('click',
+generate.addEventListener ('click',
   function calcTicket() {
-
     // price check
     if (userFullName.value == '') {
       alert("Non hai inserito il nome")
@@ -32,20 +31,50 @@ generate.addEventListener('click',
     }
     if (age.value == 'under-18') {
       ticketPrice = ticketPrice * 0.8
+
       console.log('Il bliglietto scontato di ' + userFullName.value + ' costa ' + ticketPrice.toFixed(2) + ' €')
+      document.getElementById('offer').innerHTML = "Biglietto gold";
+      document.getElementById('ticket-cost').innerHTML = ticketPrice.toFixed(2) + ' €';
     }
     if (age.value == '18') {
       ticketPrice = ticketPrice
+
       console.log('Il bliglietto scontato di ' + userFullName.value + ' costa ' + ticketPrice.toFixed(2) + ' €')
+      document.getElementById('offer').innerHTML = "Biglietto standard";
+      document.getElementById('ticket-cost').innerHTML = ticketPrice.toFixed(2) + ' €';
     }
     if (age.value == 'over-65') {
       ticketPrice = ticketPrice * 0.6
+
       console.log('Il bliglietto scontato di ' + userFullName.value + ' costa ' + ticketPrice.toFixed(2) + ' €')
+      document.getElementById('offer').innerHTML = "Biglietto silver";
+      document.getElementById('ticket-cost').innerHTML = ticketPrice.toFixed(2) + ' €';
     }
+
+    // Carriage random number from 1 to 9
+    let randomCarriage = Math.floor(Math.random() * 9) + 1;
+    document.getElementById('carriage').innerHTML = randomCarriage;
+
+    //CP-code random number from 90000 to 99999
+    let randomCP = Math.floor(Math.random() * 9999) + 90000;
+    document.getElementById('CP-code').innerHTML = randomCP;
+
+    // Reveal Your ticket
+    document.getElementById('your-ticket').style.display = "block";
+    document.getElementById('passenger').innerHTML = userFullName.value;
   }
 )
 
-
+// Reset button
+document.querySelector('.reset').addEventListener ('click',
+  function() {
+    console.log("Resetted")
+    document.getElementById("name-input").value = '';
+    document.getElementById("km-input").value = '';
+    document.getElementById("age-range").value = 'age-not-selected'
+    document.getElementById('your-ticket').style.display = "none";
+  }
+  )
 
 
 
